09b98f85aeec9848389e2878edeee3b89d5da80a
kjwook7522 <kjwook7522@gmail.com>
2021-01-17 18:44:15 +0900

    Add nextjs styled-components typescript configure

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,25 @@
-import Document from 'next/document'
+import Document, { DocumentContext, Head, Html } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
-export default class MyDocument extends Document {
 
-  // for styled-components reload faster //
-  static async getInitialProps(ctx) {
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
+
     try {
       ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-      })
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
       const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
         styles: (
           <>
-           {initialProps.styles}
-           {sheet.getStyleElement()}
+            {initialProps.styles}
+            {sheet.getStyleElement()}
           </>
         ),
       }
@@ -26,4 +28,23 @@ export default class MyDocument extends Document {
     }
   }
   /////////////////////////////////////////
+
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          <meta property="og:title" content="SSUtudy - 슈터디 " key="title" />
+          <meta property="og:description" content="공부 메이트를 찾고 싶을땐? 슈터디! &#10;숭실대 학생들의 스터디 모임을 위해 제작된 플랫폼입니다" />
+          <meta property="og:type" content="website" />
+          {/* <meta property="og:url" content="https://ssutudy.com" /> */}
+          {/* <meta property="og:image:type" content="image/jpeg" /> */}
+          {/* <meta property="og:image:width" content="400" /> */}
+          {/* <meta property="og:image:height" content="300" /> */}
+          {/* <meta property="og:image:alt" content="image description" /> */}
+          {/* <meta property="og:image" content="https://ssutudy/image/ogimage.png" /> */}
+          <meta property="og:locale" content="ko_KR" />
+        </Head>
+      </Html>
+    );
+  }
 }
