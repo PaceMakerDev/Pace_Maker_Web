8a8e1860ba32480361abe8ddeeba0b2afc8eef86
kjwook7522 <kjwook7522@gmail.com>
2021-01-17 17:29:51 +0900

    Add Button atom to component

---
+++ components/atoms/Button/Button.tsx
@@ -0,0 +1,35 @@
+import {
+  StyledSmallFillButton,
+  StyledSmallEmptyButton,
+  StyledMiddleFillButton,
+  StyledMiddleEmptyButton,
+  StyledLargeFillButton,
+  StyledLargeEmptyButton,
+} from './Button.styled';
+
+interface Props {
+  primary?: boolean;
+}
+
+export const SmallFillButton: React.FC<Props> = ({ children, primary }) => {
+  return <StyledSmallFillButton primary={primary}>{children}</StyledSmallFillButton>;
+};
+
+export const SmallEmptyButton: React.FC<Props> = ({ children, primary }) => {
+  return <StyledSmallEmptyButton primary={primary}>{children}</StyledSmallEmptyButton>;
+};
+
+export const MiddleFillButton: React.FC<Props> = ({ children, primary }) => {
+  return <StyledMiddleFillButton primary={primary}>{children}</StyledMiddleFillButton>;
+};
+
+export const MiddleEmptyButton: React.FC<Props> = ({ children, primary }) => {
+  return <StyledMiddleEmptyButton primary={primary}>{children}</StyledMiddleEmptyButton>;
+};
+export const LargeFillButton: React.FC<Props> = ({ children, primary }) => {
+  return <StyledLargeFillButton primary={primary}>{children}</StyledLargeFillButton>;
+};
+
+export const LargeEmptyButton: React.FC<Props> = ({ children, primary }) => {
+  return <StyledLargeEmptyButton primary={primary}>{children}</StyledLargeEmptyButton>;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,33 @@
+import {
+  LargeEmptyButton,
+  LargeFillButton,
+  MiddleEmptyButton,
+  MiddleFillButton,
+  SmallEmptyButton,
+  SmallFillButton,
+} from 'components/atoms/Button/Button';
+
 const Index: React.FC = () => {
-  return <div>hello</div>
-}
+  return (
+    <>
+      <div>
+        <SmallFillButton primary>전송</SmallFillButton>
+        <SmallEmptyButton primary>전송</SmallEmptyButton>
+      </div>
+      <div style={{ backgroundColor: '#0ebeaf' }}>
+        <SmallFillButton>더보기</SmallFillButton>
+        <SmallEmptyButton>더보기</SmallEmptyButton>
+      </div>
+      <div>
+        <MiddleFillButton primary>로그인</MiddleFillButton>
+        <MiddleEmptyButton primary>로그인</MiddleEmptyButton>
+      </div>
+      <div>
+        <LargeFillButton primary>스터디 추가</LargeFillButton>
+        <LargeEmptyButton primary>스터디 추가</LargeEmptyButton>
+      </div>
+    </>
+  );
+};
 
 export default Index;
