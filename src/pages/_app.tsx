62102496d06694a97b868068d4ed3da8ed1fbb5a
kjwook7522 <kjwook7522@gmail.com>
2021-01-25 01:49:42 +0900

    Implement axios default config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import type { AppProps } from 'next/app';
+import axios from 'axios';
+import { AXIOS_DEFAULT_URL } from 'common/constants/config';
 import 'common/styles/reset.css';
 import 'common/styles/globals.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  // Global axios default setting
+  axios.defaults.baseURL = AXIOS_DEFAULT_URL;
+  axios.defaults.timeout = 5000;
+  axios.defaults.headers = { 'Content-Type': 'application/json' };
+  
   return <Component {...pageProps} />;
 };
 
