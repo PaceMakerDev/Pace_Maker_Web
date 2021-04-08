45338b577d6a2ec17a278c2cd03e1c16f85b85e4
kjwook7522 <kjwook7522@gmail.com>
2021-04-09 00:44:05 +0900

    Create login template

---
+++ src/components/templates/LoginTemplate/LoginTemplate.styled.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const StyledLoginTemplate = styled.div`
+  height: 100vh;
+  padding: 2.5rem 0;
+  position: relative;
+`;
+
+export const StyledHeaderSection = styled.header`
+  margin-bottom: 2.5rem;
+`;
+
+export const StyledLoginFormSection = styled.section``;
+
+export const StyledForgotSection = styled.section`
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+`;
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import LoginTemplate from 'components/templates/LoginTemplate/LoginTemplate';
 
-const Login: React.FC = () => <div />;
+const Login: React.FC = () => <LoginTemplate headerSection={1} loginFormSection={2} forgotSection={3} />;
 
 export default Login;
