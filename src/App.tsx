7db6f981d2669856ecc9a3af4ea614ff91924d40
kjwook7522 <kjwook7522@gmail.com>
2021-05-12 00:29:06 +0900

    Add login logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import Router from 'Router';
+import axios from 'axios';
+import { useAppSelector } from 'common/reduxhooks';
+import { API_SERVER_ADDRESS } from 'common/constants';
 
 const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const authStore = useAppSelector(state => state.authReducer);
+
+  const isLogined = async (): Promise<boolean> => {
+    const accessToken = localStorage.getItem('ACCESS_TOKEN');
+    const refreshToken = localStorage.getItem('REFRESH_TOKEN');
+
+    if (!accessToken) {
+      return false;
+    }
+
+    const response = await axios.post(`${API_SERVER_ADDRESS}/auth/token/refresh`);
+    // response status에 따라서 return value를 결정 해야한다
+    return true;
+  };
 
   useEffect(() => {
     // fetch user info
 
     setTimeout(() => setIsLoading(false), 500);
   }, []);
-  return <Router isLoading={isLoading} isLogined={false} />;
+  return <Router isLoading={isLoading} isLogined={authStore.isLogined} />;
 };
 
 export default App;
--- src/components/organisms/Form/LoginForm/LoginForm.tsx
+++ src/components/organisms/Form/LoginForm/LoginForm.tsx
@@ -1,12 +1,20 @@
 import React, { useCallback, useState } from 'react';
-import { message } from 'antd';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import sha256 from 'crypto-js/sha256';
+import { useAppDispatch } from 'common/reduxhooks';
 import FullButton from 'components/atoms/Button/FullButton/FullButton';
 import RadiusInput from 'components/atoms/Input/RadiusInput/RadiusInput';
+import { setLogin } from 'actions/auth';
+import { SigninApi } from 'State';
+import { API_SERVER_ADDRESS } from 'common/constants';
 import { StyledInputWrapper } from './LoginForm.styled';
 
 const LoginForm: React.FC = () => {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
+  const dispath = useAppDispatch();
+  const history = useHistory();
 
   const handleInput: React.ChangeEventHandler<HTMLInputElement> = useCallback(event => {
     const { name, value } = event.target;
@@ -19,11 +27,29 @@ const LoginForm: React.FC = () => {
     }
   }, []);
 
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = event => {
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async event => {
     event.preventDefault();
 
-    message.warning({ content: '아직 준비중입니다', duration: 2 });
-    // write login code ...
+    const body: SigninApi = {
+      userId: id,
+      password: sha256(password).toString(),
+    };
+
+    const response = await axios.post(`${API_SERVER_ADDRESS}/auth/signin`, body);
+    const {
+      data: {
+        data: { accessToken, refreshToken },
+      },
+    } = response;
+
+    saveToken(accessToken, refreshToken);
+    dispath(setLogin());
+    history.push('/');
+  };
+
+  const saveToken = (accessToken: string, refreshToken: string): void => {
+    localStorage.setItem('ACCESS_TOCKEN', accessToken);
+    localStorage.setItem('REFRESH_TOCKEN', refreshToken);
   };
 
   return (
