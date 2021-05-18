2827841640c7253c714da84c6cefe6f1d2a1b9cf
kjwook7522 <kjwook7522@gmail.com>
2021-05-19 03:17:17 +0900

    Fix forgot section

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import LoginTemplate from 'components/templates/LoginTemplate/LoginTemplate';
 import LoginForm from 'components/organisms/Form/LoginForm/LoginForm';
 import BackHeader from 'components/molecules/BackHeader/BackHeader';
-import Link from 'components/atoms/Link/Link';
+import ForgotLinks from 'components/molecules/ForgotLinks/ForgotLinks';
 
 const Login: React.FC = () => (
   <LoginTemplate
     headerSection={<BackHeader>로그인</BackHeader>}
     loginFormSection={<LoginForm />}
-    forgotSection={
-      <Link to="/" color="#9b9b9b" fontSize="0.875rem">
-        비밀번호를 잃어버렸어요
-      </Link>
-    }
+    forgotSection={<ForgotLinks />}
   />
 );
 
