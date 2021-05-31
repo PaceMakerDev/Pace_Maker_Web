ec8cdec620a494c0f946e6aff71c8e18089f5c17
kjwook7522 <kjwook7522@gmail.com>
2021-06-01 03:21:38 +0900

    Move mobile layout size to constant

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,4 +2,6 @@ export const PRIME_COLOR_CODE = '#30D4D3';
 export const PRIME_HOVER_COLOR_CODE = '#1DB8B8';
 export const DISABLE_COLOR_CODE = '#494E4D';
 
+export const APP_WIDTH = '480px';
+
 export const API_SERVER_ADDRESS = 'http://13.209.98.129/v1';
