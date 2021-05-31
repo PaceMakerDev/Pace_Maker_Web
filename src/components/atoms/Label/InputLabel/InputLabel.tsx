a71600f9c091695c2a932fc32a16471828a6938f
kjwook7522 <kjwook7522@gmail.com>
2021-06-01 03:20:23 +0900

    Create input label component

---
+++ src/components/atoms/Label/InputLabel/InputLabel.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyledInputLabel } from './InputLabel.styled';
+
+interface Props {
+  htmlFor: string;
+  fontSize?: string;
+  color?: string;
+  required?: boolean;
+  children?: React.ReactNode;
+}
+
+const InputLabel: React.FC<Props> = ({ required, children, ...props }: Props) => (
+  <StyledInputLabel {...props}>
+    {children}
+    {required && <span className="require">*</span>}
+  </StyledInputLabel>
+);
+
+export default React.memo(InputLabel);
