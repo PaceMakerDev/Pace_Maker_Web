43da1ea8f3865d2921b8b0e7726442f89b5ec871
김재욱 <kjwook7522@gmail.com>
2021-08-05 00:15:01 +0900

    Add transform function from second to time

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -15,3 +15,37 @@ export const createBirthdayFormat = (year: string, month: string, day: string): 
 
   return result;
 };
+
+export const transformSecondToTime = (totalTime: number): string => {
+  let remainTime: number = totalTime;
+  let hour: string;
+  let minute: string;
+  let second: string;
+
+  let temp: number;
+
+  temp = Math.floor(remainTime / 3600);
+  if (temp < 10) {
+    hour = `0${temp}`;
+  } else {
+    hour = temp.toString();
+  }
+  remainTime %= 3600;
+
+  temp = Math.floor(remainTime / 60);
+  if (temp < 10) {
+    minute = `0${temp}`;
+  } else {
+    minute = temp.toString();
+  }
+  remainTime %= 60;
+
+  temp = remainTime
+  if (temp < 10) {
+    second = `0${temp}`;
+  } else {
+    second = temp.toString();
+  }
+
+  return `${hour}:${minute}:${second}`;
+};
